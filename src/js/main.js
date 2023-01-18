5abedf6789034aa79d5dd971578a1702430bdda8
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-01-18 02:40:45 +0200

    added customers section, add star rating

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
+import raterFunction from "rater-js";
+
+raterFunction({
+  element: document.querySelector(".stars"), 
+  rateCallback:function rateCallback(rating, done) {
+    this.setRating(rating); 
+    done(); 
+  },
+  starSize: 25,
+  step:0.5,
+});
+
 console.log('Джс працює!');
