5a6aae00247f0c0b03084746aa7d92c043627fdd
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-01-19 00:18:34 +0200

    added faq section

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,23 @@ raterFunction({
   step:0.5,
 });
 
-console.log('Джс працює!');
+const btn = document.querySelectorAll('.faq__item');
+
+btn.forEach(function (el) {
+  el.addEventListener('click', function(e) {
+  const collapseBox = this.querySelector('.grow');
+
+    if (this.classList.contains('open')) {
+      this.classList.remove('open');
+    } else {
+      this.classList.add('open');
+    }
+
+   if (collapseBox.clientHeight) {
+    collapseBox.style.height = 0;
+  } else {
+    var wrapper = document.querySelector('.faq__item-text');
+    collapseBox.style.height = wrapper.clientHeight + "px";
+  }
+})  
+})
